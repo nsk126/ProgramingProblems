304eb6d1aae857a497bc641da0d7c330dc5364c6
Suresh <nsk126@gmail.com>
2023-01-27 20:51:47 -0700

    50A

--- CodeForces/50A_Domino_piling.cpp
+++ CodeForces/50A_Domino_piling.cpp
@@ -2,8 +2,17 @@
 
 // https://codeforces.com/problemset/problem/50/A
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    
+    int m,n;
+
+    cin >> m >> n;
+
+    int area = m * n;
+
+    cout << int(area / 2) << endl;
+        
     return 0;
 }
