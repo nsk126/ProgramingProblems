8a4b4a80230f81ac5407338fdac05874f5aefe49
Suresh <nsk126@gmail.com>
2023-02-18 21:49:27 -0700

    added template

---
+++ CodeForces/TEMPLATE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#define ISEVEN(x) x % 2 == 0
+
+using namespace std;
+
+//
+
+void print_vector(vector<int> vec);
+void solve();
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
+
+void print_vector(vector<int> vec){
+    for(int x : vec)
+        cout << x << " ";
+    cout << endl;
+}
