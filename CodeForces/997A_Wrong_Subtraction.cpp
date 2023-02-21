d6750b25a3834d0971490730afa7715522c02371
Suresh <nsk126@gmail.com>
2023-02-20 17:19:55 -0700

    997A

--- CodeForces/997A_Wrong_Subtraction.cpp
+++ CodeForces/997A_Wrong_Subtraction.cpp
@@ -8,13 +8,33 @@
 
 using namespace std;
 
-//
+// https://codeforces.com/problemset/problem/977/A 
 
 void print_vector(vector<int> vec);
 void solve();
 
 int main(int argc, char const *argv[])
 {
+    long int n, k;
+    cin >> n >> k;
+
+    while(k--){
+
+        /* cout << "n = " << n << endl; */
+        
+        if(n<10){
+             n--; 
+             continue;     
+        }
+       
+        if(n%10==0){
+            n = n/10;
+        }else{
+            n-= 1;
+        }
+    }
+
+    cout << n << endl;
 
     return 0;
 }
@@ -24,3 +44,4 @@ void print_vector(vector<int> vec){
         cout << x << " ";
     cout << endl;
 }
+
