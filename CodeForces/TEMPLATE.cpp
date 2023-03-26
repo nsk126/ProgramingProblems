bd72d14d5708fe589f6359779a33833cfd2d2333
Suresh <nsk126@gmail.com>
2023-03-26 07:52:18 -0700

    update template

--- CodeForces/TEMPLATE.cpp
+++ CodeForces/TEMPLATE.cpp
@@ -9,12 +9,14 @@
 #define ISEVEN(x) x % 2 == 0 // is even
 #define ISDIV(x,y) x % y == 0 // is divisile
 #define ll long long
+#define ISTRUE(x) x ? "YES\n" : "NO\n"
 
 using namespace std;
 
 //
 
-void print_vector(vector<int> vec);
+void print_vector(vector<int> vec); // print int elements of a vector
+void print_strchars(string s);
 void solve();
 
 int main(int argc, char const *argv[])
@@ -32,3 +34,15 @@ void print_vector(vector<int> vec){
         cout << x << " ";
     cout << endl;
 }
+
+void print_strchars(string s){
+    for(auto x : s){
+        cout << x << endl;
+    }
+}
+
+
+/**TODO:
+ * 1. Make datatype overload for printing any vector data
+ * 3. add a reverse string function
+*/
