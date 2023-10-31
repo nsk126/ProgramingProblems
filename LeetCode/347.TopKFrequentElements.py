75b91cb000e3330a369f56e33acd4de90d3e72e1
Suresh <nsk126@gmail.com>
2023-10-30 17:38:59 -0700

    347 python

--- LeetCode/347.TopKFrequentElements.py
+++ LeetCode/347.TopKFrequentElements.py
@@ -1,20 +1,39 @@
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
 
-        hashmap = {}
-        ret = []
+        hmap = {}
+        ans = []
+        # goal: find top k elements
+        # go thru list, make a hashmap - key: num, val: freq
+        for num in nums:
+            hmap[num] = 1 + hmap.get(num, 0)
 
-        for x in nums:
-            if x not in hashmap:
-                hashmap[x] = 1
-            else:
-                hashmap[x] += 1
-        return
+        karr = []
+        varr = []
+        for kk, vv in hmap.items():
+            karr.append(kk)
+            varr.append(vv)
+
+        while k > 0:
+            # find max in varr
+            # use index mapping to find key
+            # overwrite max to 0
+            c = varr.index(max(varr))
+            ans.append(karr[c]) # key of interest
+            varr[c] = 0 # overwrite max value to 0 - to get next max value
+
+            # decrement k
+            k -= 1
+
+        return ans
+
 
 
 sol = Solution()
 
-nums, k = [1,1,1,2,2,3], 2
+# nums, k = [1,1,1,2,2,3], 2
+nums, k = [1], 1
+
 op = sol.topKFrequent(nums, k)
 
 print(op)
