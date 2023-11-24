6157bc0869aedaa9b8435a8773dcbfb07ab6da22
Suresh <nsk126@gmail.com>
2023-11-23 19:30:26 -0700

    543

---
+++ LeetCode/543.DiameterofBinaryTree.py
@@ -0,0 +1,25 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        depth = [0]
+        def dfs(root):
+            if root is None:
+                return -1
+
+            l = dfs(root.left)
+            r = dfs(root.right)
+            depth[0] = max(depth[0], 2 + l + r)
+
+            return 1 + max(l,r)
+
+        dfs(root)
+
+        return depth[0]
